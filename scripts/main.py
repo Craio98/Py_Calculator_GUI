74273280022245df77e9b7a2848d9691616ecca0
Craio98 <116519294+Craio98@users.noreply.github.com>
2022-12-17 18:51:22 +0200

    Creating main file

---
+++ scripts/main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+window=tk.Tk()
+window.title("Calculator v1.01")
+frame=tk.Frame(master=window,bg="yellow",padx=10)
+frame.pack()
+ent_num=tk.Entry(relief=SUNKEN, borderwidth=3,width=30)
+entry.grid(row=0,column=0, columnspan=3, ipady=2,pady=2)
+
+button_1=tk.Button(master=frame,text="1")
+button_1.grid(row=1, column=0, pady=2)
+button_2=tk.Button(master=frame,text="1")
+utton_1.grid(row=1, column=1, pady=2)
+button_3=tk.Button(master=frame,text="1")
+utton_1.grid(row=1, column=2, pady=2)
+button_4=tk.Button(master=frame,text="1")
+utton_1.grid(row=1, column=3, pady=2)
+button_5=tk.Button(master=frame,text="1")
+utton_1.grid(row=1, column=4, pady=2)
+button_6=tk.Button(master=frame,text="1")
+utton_1.grid(row=1, column=5, pady=2)
+button_7=tk.Button(master=frame,text="1")
+utton_1.grid(row=1, column=6, pady=2)
+button_8=tk.Button(master=frame,text="1")
+utton_1.grid(row=1, column=7, pady=2)
+button_9=tk.Button(master=frame,text="1")
+utton_1.grid(row=1, column=1, pady=2)
+
+
+
+window.mainloop()
+
